e7f763e894264e30eea44566812c4646144bca79
Gambitou <claudiosalas245@gmail.com>
2026-05-08 11:10:18 -0400

    Actualizar ejercicios.js

---
+++ ejercicios.js
@@ -0,0 +1,47 @@
+//inicio del codigo donde defino variables de mi nombre y apellido para poder mostrarlas por pantalla
+let nombre = "claudio";
+let apellido = "salas";
+console.log(nombre , apellido);
+//aqui defino una variable de tipo numero y hago una operacion matematica con ella para mostrar el resultado por pantalla
+let numero1 = 2;
+console.log((numero1 + 5) * 3);
+//ahora defino una variable de tipo string para mostrar mi comida favorita por pantalla
+let comidaFav = "pizza";
+console.log("mi comida favorita es " + comidaFav);
+//ahora defino dos variables de tipo numero para hacer operaciones matematicas con ellas y mostrar los resultados por pantalla
+let a = 10;
+let b = 3;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a % b);
+a = a.toString();//convierte el numero a string
+b = b.toString();//convierte el numero a string
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a % b);
+//creare una variable de edad 15 años y luego hare una condicion para ver si esta en un rango especifico
+let edad = 15;
+console.log(edad >= 10 && edad <= 18);
+//ahora creare una variable con el numero 8 y hare lo mismo del punto de arriba
+let numero = 8;
+console.log(numero >= 5 && numero <= 10);
+//ahora creare una variable con nota 6 y vere si es igual a 7
+let nota = 6;
+console.log(nota == 7);
+//creare una variable que sea de temperatura de 25 grados y vere si es mayor o igual a 30 grados
+let temperatura = 25;
+console.log(temperatura >= 30);
+//ahora creare dos numeros y imprimire si uno es mayor que el otro
+let num1 = 15;
+let num2 = 20;
+console.log(num1 > num2);
+//punto numero 10
+let edad=17;
+let tienePermiso=true;
+console.log(edad >= 18 || tienePermiso);
+//punto numero 11
+let usuario="admin";
+let password="1234";
+console.log(usuario === "admin" && password === "1234");
